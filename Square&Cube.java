e9c31a86ec73f1d583071d8984f657556416963c
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2023-08-20 11:27:00 -0500

    Create Square&Cube.java
    Java code to, given a keyboard double number, calculate the integer  square & cube from that number

---
+++ Square&Cube.java
@@ -0,0 +1,15 @@
+import java.lang.Math;
+import java.util.Scanner;
+public class sqCb {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        double number, sq, cb, square=2, cube=3;
+        System.out.println("Ingrese el numero a operar: ");
+        number =input.nextInt();
+        sq=Math.pow( number,square);
+        cb=Math.pow(number, cube);
+        System.out.println("El cuadrado de "+(int) number+" es: "+(int) sq);
+        System.out.println("El cubo de "+(int) number+" es: "+(int) cb);
+
+    }
+}
