d6b5c8af3d38481d412d629e7eaa790ebb23f699
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2023-08-27 18:23:06 -0500

    Update TriangleArea.java

--- TriangleArea.java
+++ TriangleArea.java
@@ -1,7 +1,7 @@
 import java.lang.Math;
 public class TriangleArea {
     double ladoA, ladoB, ladoC, SemPer;
-    Area(double ladoA, double ladoB, double ladoC, double SemPer){
+    TriangleArea(double ladoA, double ladoB, double ladoC, double SemPer){
         this.ladoA=ladoA;
         this.ladoB=ladoB;
         this.ladoC=ladoC;
