d7b4a6dbfb6234012a45ac71b7f2c5f39fc1c1df
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2023-09-12 14:39:00 -0500

    Update and rename EquilateralTriangleCalcs.java to EquilateralTriangleCalcsForm.java

--- EquilateralTriangleCalcsForm.java
+++ EquilateralTriangleCalcsForm.java
@@ -8,12 +8,12 @@ package com.mycompany.forms;
  *
  * @author Usuario
  */
-public class EquilateralTriangleCalcs extends javax.swing.JFrame {
+public class EquilateralTriangleCalcsForm extends javax.swing.JFrame {
 
     /**
      * Creates new form Form
      */
-    public EquilateralTriangleCalcs() {
+    public EquilateralTriangleCalcsForm() {
         initComponents();
     }
 
@@ -190,21 +190,23 @@ public class EquilateralTriangleCalcs extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(EquilateralTriangleCalcs.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(EquilateralTriangleCalcsForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(EquilateralTriangleCalcs.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(EquilateralTriangleCalcsForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(EquilateralTriangleCalcs.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(EquilateralTriangleCalcsForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(EquilateralTriangleCalcs.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(EquilateralTriangleCalcsForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new EquilateralTriangleCalcs().setVisible(true);
+                new EquilateralTriangleCalcsForm().setVisible(true);
             }
         });
     }
