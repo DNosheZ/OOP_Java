a7e51294ada61f5eed99b962b822374562e7489a
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2023-09-12 15:26:11 -0500

    Create MaxMin.java

---
+++ MaxMin.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.maxmin;
+
+/**
+ *
+ * @author Usuario
+ */
+public class MaxMin {
+
+    public static void main(String[] args) {
+        MaxMinForm form = new MaxMinForm();
+        form.setVisible(true);
+    }
+}
