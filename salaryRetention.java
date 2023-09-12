4d65a274432901dbbf93619c8c6c18ac572e4a04
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2023-09-12 10:35:16 -0500

    Create salaryRetention.java

---
+++ salaryRetention.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.formsalary;
+
+/**
+ *
+ * @author Usuario
+ */
+public class salaryRetention {
+
+    public static void main(String[] args) {
+        salaryRetentionForm form = new salaryRetentionForm();
+        form.setVisible(true);
+    }
+}
