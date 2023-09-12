f2d5fc1ffe4f7f23951919724b5f69bf782c74d5
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2023-09-12 14:38:01 -0500

    Create EquilateralTriangleCalcsF.java

---
+++ EquilateralTriangleCalcsF.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.forms;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EquilateralTriangleCalcsF {
+
+    public static void main(String[] args) {
+        EquilateralTriangleCalcsForm form = new EquilateralTriangleCalcsForm();
+        form.setVisible(true);
+    }
+}
