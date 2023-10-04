b56b3e9028cbeeb43599789a32e657b95037b100
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2023-10-04 13:01:33 -0500

    Create VentanaPrincipal.java

---
+++ VentanaPrincipal.java
@@ -0,0 +1,150 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package com.mycompany.personas;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+/**
+ *
+ * @author Usuario
+ */
+public class VentanaPrincipal extends javax.swing.JFrame implements ActionListener  {
+    private ListaPersonas lista;
+    private Container contenedor;
+    private JLabel nombre, apellidos, telefono, direccion;
+    private JTextField campoNombre, campoApellidos, campoTelefono, campoDireccion;
+    private JButton añadir, eliminar, borrarLista;
+    private JList listaNombres;
+    private DefaultListModel modelo;
+    private JScrollPane scrollLista;
+    public VentanaPrincipal(){
+        lista = new ListaPersonas();
+        inicio();
+        setTitle("Personas");
+        setSize(270,350);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+    }
+    private void inicio() {
+        contenedor = getContentPane();
+        contenedor.setLayout(null);
+        nombre = new JLabel();
+        nombre.setText("Nombre:");
+        nombre.setBounds(20, 20, 135, 23);
+        campoNombre = new JTextField();
+        campoNombre.setBounds(105, 20, 135, 23);
+        apellidos = new JLabel();
+        apellidos.setText("Apellidos:");
+        apellidos.setBounds(20, 50, 135, 23);
+        campoApellidos = new JTextField();
+        campoApellidos.setBounds(105, 50, 135, 23);
+        telefono = new JLabel();
+        telefono.setText("Teléfono:");
+        telefono.setBounds(20, 80, 135, 23);
+        campoTelefono = new JTextField();
+        campoTelefono.setBounds(105, 80, 135, 23);
+        direccion = new JLabel();
+        direccion.setText("Dirección:");
+        direccion.setBounds(20, 110, 135, 23);
+        campoDireccion = new JTextField();
+        campoDireccion.setBounds(105, 110, 135, 23);
+        añadir = new JButton();
+        añadir.setText("Añadir");
+        añadir.setBounds(105, 150, 80, 23);
+        añadir.addActionListener(this);
+        eliminar= new JButton();
+        eliminar.setText("Eliminar");
+        eliminar.setBounds(20, 280, 80, 23);
+        eliminar.addActionListener(this);
+        borrarLista= new JButton();
+        borrarLista.setText("Borrar Lista");
+        borrarLista.setBounds(120, 280, 120, 23);
+        borrarLista.addActionListener(this);
+        listaNombres = new JList();
+        listaNombres.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        modelo = new DefaultListModel();
+        scrollLista = new JScrollPane();
+        scrollLista.setBounds(20, 190 ,220, 80);
+        scrollLista.setViewportView(listaNombres);
+        contenedor.add(nombre);
+        contenedor.add(campoNombre);
+        contenedor.add(apellidos);
+        contenedor.add(campoApellidos);
+        contenedor.add(telefono);
+        contenedor.add(campoTelefono);
+        contenedor.add(direccion);
+        contenedor.add(campoDireccion);
+        contenedor.add(añadir);
+        contenedor.add(eliminar);
+        contenedor.add(borrarLista);
+        contenedor.add(scrollLista);
+    }
+    /**
+    * Método que gestiona los eventos generados en la ventana principal
+    */
+    @Override
+    public void actionPerformed(ActionEvent evento) {
+        if (evento.getSource() == añadir) {
+        añadirPersona();
+        }
+        if (evento.getSource() == eliminar) {
+            eliminarNombre(listaNombres.getSelectedIndex() );
+        }
+        if (evento.getSource() == borrarLista) {
+            borrarLista();
+        }
+    }
+    private void añadirPersona() {
+        Persona p = new Persona(campoNombre.getText(), 
+        campoApellidos.getText(), 
+        campoTelefono.getText(), campoDireccion.getText());
+        lista.añadirPersona(p);
+        String elemento = campoNombre.getText() + "-" + 
+        campoApellidos.getText() + "-" + campoTelefono.getText() + "-" + campoDireccion.getText();
+        modelo.addElement(elemento); /* Se agrega el texto con los 
+        datos de la persona al JList */
+        listaNombres.setModel(modelo);
+        // Se colocan todos los campos de texto nulos
+        campoNombre.setText("");
+        campoApellidos.setText("");
+        campoTelefono.setText("");
+        campoDireccion.setText("");
+    }
+    private void eliminarNombre(int indice) {
+        if (indice >= 0) { 
+            modelo.removeElementAt(indice);
+            lista.eliminarPersona(indice);
+        } else { 
+            JOptionPane.showMessageDialog(null, "Debe seleccionar un elemento","Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    private void borrarLista() {
+        lista.borrarLista();
+        modelo.clear();
+    }    
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>                        
+
+    // Variables declaration - do not modify                     
+    // End of variables declaration                   
+}
