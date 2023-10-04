f0eaaca25bd8546b3e12c7e1f199a1dc041dc712
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2023-10-04 13:01:05 -0500

    Create Personas.java

---
+++ Personas.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.personas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Personas {
+   public static void main(String[] args) {
+    VentanaPrincipal miVentanaPrincipal;
+    miVentanaPrincipal= new VentanaPrincipal();
+    miVentanaPrincipal.setVisible(true);
+   } 
+}
