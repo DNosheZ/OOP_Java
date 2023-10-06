d870720aeea06b1b8a58086a88a14e37059de918
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2023-10-06 18:52:33 -0500

    Update ListaPersonas.java

--- ListaPersonas.java
+++ ListaPersonas.java
@@ -4,32 +4,19 @@
  */
 package com.mycompany.personas;
 import java.util.*;
-/**
-* Esta clase denominada ListaPersonas define un vector de Personas.
-* @version 1.2/2020
-*/
+
 public class ListaPersonas {
     Vector listaPersonas;
     public ListaPersonas() {
         listaPersonas = new Vector();
     }
-    /**
-    * Método que permite agregar una persona al vector de personas
-    * @param p Parámetro que define la persona a agregar al vector de 
-    * personas
-    */
     public void añadirPersona(Persona p) {
         listaPersonas.add(p);
     }
-    /**
-    * Método que permite eliminar una persona del vector de personas
-    * @param i Parámetro que define la posición a eliminar en el vector 
-    * de personas
-    */
     public void eliminarPersona(int i) {
         listaPersonas.removeElementAt(i);
     }
     public void borrarLista() {
-    listaPersonas.removeAllElements();
+        listaPersonas.removeAllElements();
     }
 }
