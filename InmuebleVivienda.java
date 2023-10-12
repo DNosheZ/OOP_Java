8e5275d021594cec3a6ae214d5bac4920e250339
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2023-10-12 16:15:49 -0500

    Update InmuebleVivienda.java

--- InmuebleVivienda.java
+++ InmuebleVivienda.java
@@ -9,35 +9,44 @@ package com.mycompany.agenciainmueble;
  * @author Usuario
  */
 /**
-* Esta clase denominada CasaUrbana modela un tipo específico de casa 
-* destinada para la vivienda localizada en el sector urbano.
+* Esta clase denominada InmuebleVivienda modela un inmueble
+* destinado para la vivienda con atributos como el número de
+* habitaciones y el número de baños que posee
 * @version 1.2/2020
 */
 public class InmuebleVivienda extends Inmueble {
+    /* Atributo que identifica el número de habitación de un inmueble
+    para vivienda */
+    protected int numeroHabitaciones;
+    /* Atributo que identifica el número de baños de un inmueble para
+    vivienda */
+    protected int numeroBaños;
     /**
-    * Constructor de la clase CasaUrbana
-    * @param identificadorInmobiliario Parámetro que define el 
-    * identificador inmobiliario de una casa urbana
-    * @param area Parámetro que define el área de una casa urbana
-    * @param direccion Parámetro que define la dirección donde se 
-    * encuentra localizada una casa urbana
-    * @param numeroHabitaciones Parámetro que define el número de 
-    * habitaciones que tiene una casa urbana
-    * @param numeroBaños Parámetro que define el número de baños 
-    * que tiene una casa urbana
-    * @param numeroPisos Parámetro que define el número de pisos 
-    * que tiene una casa urbana
+    * Constructor de la clase InmuebleVivienda
+    * @param identificadorInmobiliario Parámetro que define el
+    * identificador inmobiliario de un inmueble para la vivienda
+    * @param área Parámetro que define el área de un inmueble para la
+    * vivienda
+    * @param dirección Parámetro que define la dirección donde se
+    * encuentra localizado un inmueble para la vivienda
+    * @param númeroHabitaciones Parámetro que define el número de
+    * habitaciones que tiene un inmueble para la vivienda
+    * @param númeroBaños Parámetro que define el número de baños
+    * que tiene un inmueble para la vivienda
     */
-    public CasaUrbana(int identificadorInmobiliario, int area, String 
-        direccion, int numeroHabitaciones, int numeroBaños, int 
-        numeroPisos) {
-        // Invoca al constructor de la clase padre
-        super(identificadorInmobiliario, area, direccion, numeroHabitaciones, numeroBaños, numeroPisos);
+    public InmuebleVivienda(int identificadorInmobiliario, int area, String direccion, int numeroHabitaciones, int numeroBaños) {
+        super(identificadorInmobiliario, area, direccion); /* Invoca al
+        constructor de la clase padre */
+        this.numeroHabitaciones = numeroHabitaciones;
+        this.numeroBaños = numeroBaños;
     }
     /**
-    * Método que muestra en pantalla los datos de una casa urbana
+    * Método que muestra en pantalla los datos de un inmueble para la
+    * vivienda
     */
     void imprimir() {
-        super.imprimir(); // Invoca al método imprimir de la clase padre
+    super.imprimir(); // Invoca al método imprimir de la clase padre
+        System.out.println("Número de habitaciones = " +numeroHabitaciones);
+        System.out.println("Número de baños = " + numeroBaños);
     }
 }
