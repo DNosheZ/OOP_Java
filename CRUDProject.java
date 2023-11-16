f4a3bf8bcb3e56cc231e2d686c328895b8203052
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2023-11-16 16:03:21 -0500

    Create CRUDProject.java

---
+++ CRUDProject.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.crudproject;
+
+/**
+ *
+ * @author Usuario
+ */
+public class CRUDProject {
+
+    public static void main(String[] args) {
+        VentanaPrincipalCRUD form = new VentanaPrincipalCRUD();
+        form.setVisible(true);
+    }
+}
